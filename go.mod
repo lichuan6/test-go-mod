b6b5a7d353cbeac923dab0029ca8302940a57b4e
x <x>
2021-03-16 12:01:55 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lichuan6/test-go-mod
+
+go 1.16
+
+require github.com/lichuan6/go-mod-test v0.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	hello "github.com/lichuan6/go-mod-test"
+)
+
+func main() {
+	fmt.Println(hello.Hello())
+}
